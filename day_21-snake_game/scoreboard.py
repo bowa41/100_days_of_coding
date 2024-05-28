2101aeec5c36feb38f7b4efee1791e19614f8f90
bowa41 <bowa41@att.net>
2024-05-28 12:09:57 -0700

    Update to track high score

--- day_21-snake_game/scoreboard.py
+++ day_21-snake_game/scoreboard.py
@@ -21,11 +21,10 @@ class Scoreboard(Turtle):
 
     def reset(self):
         if self.score > self.high_score:
+            self.high_score = self.score
             with open("data.txt", mode="w") as file:
                 file.write(f"{self.score}")
         self.score = 0
-        with open("data.txt") as file:
-            self.high_score = int(file.read())
         self.update_scoreboard()
 
     def increase_score(self):
