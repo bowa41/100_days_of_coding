30e9650ed25d1b51ed8e8b7a711eb48e9d33f911
bowa41 <bowa41@att.net>
2024-05-28 12:07:32 -0700

    Update to track high score

---
+++ day_21-snake_game/scoreboard.py
@@ -0,0 +1,33 @@
+from turtle import Turtle
+ALIGNTMENT = "center"
+FONT = ("Courier", 16, "normal")
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.ht()
+        self.penup()
+        self.color("white")
+        self.teleport(0, 270)
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
+        self.score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score = {self.score} High Score = {self.high_score}", move=False, align=ALIGNTMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            with open("data.txt", mode="w") as file:
+                file.write(f"{self.score}")
+        self.score = 0
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
